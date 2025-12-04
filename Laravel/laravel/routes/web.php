1d7b810d0fa00ef339eb438e21ca51825e0b7848
vjp-davidLF <dlopezf03@informatica.iesvalledeljerteplasencia.es>
2025-12-04 18:34:52 +0100

    wre

--- Laravel/laravel/routes/web.php
+++ Laravel/laravel/routes/web.php
@@ -5,3 +5,24 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+Route::get('/users', function () {
+    return view('usuarios');
+}) -> name('usuarios');
+
+
+/* http://localhost/datos/ */
+
+Route::view('datos', 'usuarios');
+
+Route::get('cliente/{id}', function($id) {
+    return('Cliente con el id: ' . $id);
+});
+
+Route::get('cliente/{id?}', function($id = 1) {
+    return ('Cliente con el id: ' . $id);
+}) -> where('id', '[0-9]+');
+
+Route::view('datos', 'usuarios', ['id' => 5446]);
